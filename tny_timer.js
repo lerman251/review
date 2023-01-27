24c161619e14a64629b3eb21aa7ccb254f7b3129
LThompson1s <LThompson1s@capetigers.com>
2023-01-27 08:43:35 -0600

    accually finished

--- tny_timer.js
+++ tny_timer.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
@@ -10,43 +10,28 @@
 
 */
 showClock();
-setInterval("showClock()", 1000)
+setInterval("showClock()", 1000);
 
 function showClock() {
 
-   //store time and date
    var thisDay = new Date();
    var localDate = thisDay.toLocaleDateString();
    var localTime = thisDay.toLocaleTimeString();
-   
-   //display the time and date
+
    document.getElementById("currentTime").innerHTML = localDate + "<br/>" + localTime;
-   //Calculate how many days left until July 4th
 
-   var j4Date = new Date("July 4, 2020 21:00:00");
-   var nextjDate = thisDay.getFullYear() + 1;
-   j4Date.setFullYear(nextjDate);
-   var daysLeft = (j4Date - thisDay)/(1000*60*60*24);
+  var j4Date = nextJuly4(thisDay);
+  j4Date.setHours(21);
 
-   //Calculate the hours left in the current day
+   var days = (j4Date - thisDay)/(1000*60*60*24);
+   var hrs = (days - Math.floor(days)) * 24;
+   var mins = (hrs - Math.floor(hrs))*60;
+   var secs = (mins - Math.floor(mins))*60;
 
-   var hrsLeft = (daysLeft - Math.floor(daysLeft)) * 24;
-
-   //Calculate the minutes and seconds left in the current day
-
-   var minsLeft = (hrsLeft - Math.floor(hrsLeft))*60;
-   var secsLeft = (minsLeft - Math.floor(minsLeft))*60;
-
-//Display the time left until 4th of July
-
-   document.getElementById("dLeft").textContent= Math.floor(daysLeft);
-   document.getElementById("hLeft").textContent= Math.floor(hrsLeft);
-   document.getElementById("mLeft").textContent= Math.floor(minsLeft);
-   document.getElementById("sLeft").textContent= Math.floor(secsLeft);
-
-//Call the next july function
-var j4Date = nextJuly4(thisDay);
-
+   document.getElementById("dLeft").textContent= Math.floor(days);
+   document.getElementById("hLeft").textContent= Math.floor(hrs);
+   document.getElementById("mLeft").textContent= Math.floor(mins);
+   document.getElementById("sLeft").textContent= Math.floor(secs);
 }
    
 function nextJuly4(currentDate) {
@@ -56,3 +41,4 @@ function nextJuly4(currentDate) {
    if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
    return jDate;
 }
+
